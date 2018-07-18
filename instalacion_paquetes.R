3c93775a7ec5aba9c66e4a18eb82ab3484a51ee6
carlostechinnovation <carlosandresgarcia1986@hotmail.com>
2018-07-19 00:40:31 +0200

    Instalacion paquetes

---
+++ instalacion_paquetes.R
@@ -0,0 +1,27 @@
+#Instalacion de los paquetes necesarios
+
+# install.packages("rmarkdown")
+# install.packages("RMySQL")
+# install.packages("SuperLearner")
+# install.packages("e1071") #SVM
+# install.packages("rpart") #Regression trees
+# install.packages("arm")
+# install.packages("randomForest")
+# install.packages("bartMachine")
+# install.packages("xgboost")
+# install.packages("kernlab")
+# install.packages("ranger")
+# install.packages("curl")
+# install.packages("rio")
+# install.packages("ggplot2")
+# install.packages("car")
+# install.packages("cluster")
+# install.packages("MASS")
+# install.packages("caret")
+# install.packages("rpart") #Algoritmo para árbol de decisión
+# install.packages("rpart.plot") #Plot de árbol de decisión
+# install.packages("C50")
+# install.packages("klaR")
+# install.packages("dplyr")
+# install.packages("mlbench")
+
